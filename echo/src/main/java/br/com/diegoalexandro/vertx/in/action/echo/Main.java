e74fcd53ea11e02d0153f6cd366c8557c9fabc88
Diego Alexandro de Oliveira <diegoalexandrooliveira@gmail.com>
2021-06-29 16:15:29 -0300

    add echo project

--- echo/src/main/java/br/com/diegoalexandro/vertx/in/action/echo/Main.java
+++ echo/src/main/java/br/com/diegoalexandro/vertx/in/action/echo/Main.java
@@ -1,7 +1,38 @@
 package br.com.diegoalexandro.vertx.in.action.echo;
 
-public class Main {
-    public static void main(String[] args) {
+import io.vertx.core.Vertx;
+import io.vertx.core.net.NetSocket;
 
+public class Main {
+
+    private static int numberOfConnections = 0;
+
+    public static void main(String[] args) {
+        var vertx = Vertx.vertx();
+
+        vertx.createNetServer()
+                .connectHandler(Main::handleNewClient)
+                .listen(3000);
+
+        vertx.setPeriodic(5000, id -> System.out.println(getConnections()));
+
+        vertx.createHttpServer()
+                .requestHandler(request -> request.response().end(getConnections()))
+                .listen(8080);
+    }
+
+    private static void handleNewClient(final NetSocket netSocket) {
+        numberOfConnections++;
+        netSocket.handler(buffer -> {
+            netSocket.write(buffer);
+            if (buffer.toString().equals("/quit\n")) {
+                netSocket.close();
+            }
+        });
+        netSocket.closeHandler(v -> numberOfConnections--);
+    }
+
+    private static String getConnections() {
+        return String.format("We now have %s connections", numberOfConnections);
     }
 }
