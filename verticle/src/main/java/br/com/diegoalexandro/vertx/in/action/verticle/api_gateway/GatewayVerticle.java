f9e320377a5f906aa48e19e942adae8aef4c10f8
Diego Alexandro de Oliveira <diegoalexandrooliveira@gmail.com>
2021-07-11 10:07:42 -0300

    change implementation to Future

--- verticle/src/main/java/br/com/diegoalexandro/vertx/in/action/verticle/api_gateway/GatewayVerticle.java
+++ verticle/src/main/java/br/com/diegoalexandro/vertx/in/action/verticle/api_gateway/GatewayVerticle.java
@@ -1,18 +1,19 @@
 package br.com.diegoalexandro.vertx.in.action.verticle.api_gateway;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.predicate.ResponsePredicate;
 import io.vertx.ext.web.codec.BodyCodec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class GatewayVerticle extends AbstractVerticle {
 
@@ -27,43 +28,44 @@ public class GatewayVerticle extends AbstractVerticle {
                 .listen(8080);
     }
 
-    private void handleRequest(HttpServerRequest httpServerRequest) {
-        List<JsonObject> responses = new ArrayList<>();
-        var counter = new AtomicInteger(0);
-        for (var i = 0; i < 3; i++) {
-            webClient
-                    .get(3000 + i, "localhost", "/")
-                    .expect(ResponsePredicate.SC_SUCCESS)
-                    .as(BodyCodec.jsonObject())
-                    .send(ar -> {
-                        log.info("Chamou o serviço de temperatura.");
-                        if (ar.succeeded()) {
-                            responses.add(ar.result().body());
-                        } else {
-                            log.error("Problema ao chamar o serviço de temperatura", ar.cause());
-                        }
-                        if (counter.incrementAndGet() == 3) {
-                            JsonObject data = new JsonObject()
-                                    .put("data", new JsonArray(responses));
-                            sendToSnapshot(httpServerRequest, data);
-                        }
-                    });
-        }
+    private Future<JsonObject> fetchTemperature(int port) {
+        return webClient
+                .get(port, "localhost", "/")
+                .expect(ResponsePredicate.SC_SUCCESS)
+                .as(BodyCodec.jsonObject())
+                .send()
+                .map(HttpResponse::body);
     }
 
-    private void sendToSnapshot(HttpServerRequest httpServerRequest, JsonObject data) {
-        webClient
+    private void handleRequest(HttpServerRequest httpServerRequest) {
+
+        CompositeFuture.all(
+                fetchTemperature(3000),
+                fetchTemperature(3001),
+                fetchTemperature(3002))
+                .flatMap(this::sendToSnapshot)
+                .onSuccess(data -> httpServerRequest.response()
+                        .putHeader("Content-Type", "application/json")
+                        .end(data.encode()))
+                .onFailure(err -> {
+                    log.error("Problema ao chamar o serviço de temperatura", err);
+                    httpServerRequest.response().setStatusCode(500).end();
+                });
+
+    }
+
+    private Future<JsonObject> sendToSnapshot(CompositeFuture temperatures) {
+        List<JsonObject> allTemps = temperatures.list();
+        JsonObject data = new JsonObject()
+                .put("data", new JsonArray()
+                        .add(allTemps.get(0))
+                        .add(allTemps.get(1))
+                        .add(allTemps.get(2)));
+        return webClient
                 .post(4000, "localhost", "/")
                 .expect(ResponsePredicate.SC_SUCCESS)
-                .sendJsonObject(data, ar -> {
-                    if (ar.succeeded()) {
-                        httpServerRequest.response()
-                                .putHeader("Content-Type", "application/json")
-                                .end(data.encode());
-                    } else {
-                        log.error("Falha ao enviar dados para o Snapshot Service", ar.cause());
-                        httpServerRequest.response().setStatusCode(500).end();
-                    }
-                });
+                .as(BodyCodec.jsonObject())
+                .sendJsonObject(data)
+                .map(response -> data);
     }
 }
